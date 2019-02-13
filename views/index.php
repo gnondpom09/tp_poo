e02846fb3db56acde0b58d3d3105d2fc349ad17e
VEDIE Sandra <sandra.vedie@gmail.com>
2019-02-13 15:23:21 +0100

    mise à jour

--- views/index.php
+++ views/index.php
@@ -1,43 +1,3 @@
-<<<<<<< HEAD
-<?php
-echo'
-
-<div class="container">
-    <div class="grid-1 callout ">
-        <form class="callout text-center">
-         <div class="floated-label-wrapper">
-             <label for="full-name">Auteur</label>
-            <input type="text" id="full-name" name="full name input" placeholder="Full name">
-        </div>
-        <div class="floated-label-wrapper">
-            <label for="email">Type</label>
-             <input type="text" id="email" name="email input" placeholder="Email">
-         </div>
-  <div class="floated-label-wrapper">
-    <label for="pass">Description</label>
-    <input type="text" id="pass" name="password input" placeholder="Password">
-  </div>
-  <input class="button expanded" type="submit" value="Valider">
-</form>
-
-
-
-
-
-    </div>
-
-    <div class="grid-2 callout">
-    Square 2
-    </div>
-
-</div>
-';
-
-
-
-
-=======
-
 <div class="container">
     <div class="grid-1 callout colums-small-6">
         <form class="callout text-center">
@@ -62,7 +22,3 @@ echo'
     </div>
 
 </div>
-
-
-
->>>>>>> da4b93e650a21a5f3ac624cc1af8f924dada36a9
