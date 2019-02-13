6da5fbf37092b9fc5554eaa528f899390b425a5f
myasminem <myasmine.mhamdi@gmail.com>
2019-02-13 16:20:47 +0100

    page de upload + repertoire multimédia

--- views/index.php
+++ views/index.php
@@ -3,15 +3,15 @@
         <form class="callout text-center">
             <div class="floated-label-wrapper">
                 <label for="full-name">Auteur</label>
-                <input type="text" id="full-name" name="full name input" placeholder="Full name">
+                <input type="text" id="full-name" name="full name input" placeholder="Auteur">
             </div>
             <div class="floated-label-wrapper">
                 <label for="email">Type</label>
-                <input type="text" id="email" name="email input" placeholder="Email">
+                <input type="text" id="email" name="email input" placeholder="Type">
             </div>
             <div class="floated-label-wrapper">
                 <label for="pass">Description</label>
-                <input type="text" id="pass" name="password input" placeholder="Password">
+                <input type="text" id="pass" name="password input" placeholder="Description">
             </div>
             <input class="button expanded" type="submit" value="Valider">
         </form>
---
+++ views/upload.php
@@ -0,0 +1,14 @@
+<!-- formulaire de  -->
+
+
+        <form class="callout text-center" enctype="multipart/form-data">
+         <div class="floated-label-wrapper">
+             <label for="Fichier">Fichier</label>
+            <input type="file" id="fichier" name="fichier" placeholder="Fichier">
+         </div>
+         <div class="floated-label-wrapper">
+            <label for="description">Description</label>
+             <input type="text" id="description" name="description" placeholder="Description">
+         </div>
+            <input class="button expanded" type="submit" value="Ajouter">
+        </form>
