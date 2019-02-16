b33214e4f54f3e18fc2385fe78f85c9ada82a72c
VEDIE Sandra <sandra.vedie@gmail.com>
2019-02-16 17:31:31 +0100

    login

--- Proj2_NoOBJECT/log.php
+++ Proj2_NoOBJECT/log.php
@@ -10,8 +10,6 @@ if (isset($_POST['login']) && isset($_POST['pwd'])) {
 	if ($login_valide == $_POST['login'] && $pwd_valide == $_POST['pwd']) {
 		// dans ce cas, tout est ok, on peut démarrer notre session
 
-		// on la démarre :)
-		session_start ();
 		// on enregistre les paramètres de notre visiteur comme variables de session ($login et $pwd) (notez bien que l'on utilise pas le $ pour enregistrer ces variables)
 		$_SESSION['login'] = $_POST['login'];
 		$_SESSION['pwd'] = $_POST['pwd'];
