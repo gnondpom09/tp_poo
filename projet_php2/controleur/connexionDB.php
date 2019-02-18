b9f52764fbbe08eeeabd2e03d0b39b3322bcaa53
stag <myasmine.mhamdi@gmail.com>
2019-02-18 02:28:21 +0100

    modif

--- projet_php2/controleur/connexionDB.php
+++
@@ -1,9 +0,0 @@
-<?php
-//Identifiants pour l'accès à la base de données
-$host='localhost';
-$dbname='multimedia';
-$usr='root';
-$pwd='1234512345';
-//connexion à la base de données
-connectDB($host,$dbname,$usr,$pwd);
-$connect=connectDB($host,$dbname,$usr,$pwd);
