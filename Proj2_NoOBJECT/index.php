b57b06d0f96063d8c04909c8af34fb95bee93f70
VEDIE Sandra <sandra.vedie@gmail.com>
2019-02-18 10:55:22 +0100

    recherche

---
+++ Proj2_NoOBJECT/index.php
@@ -0,0 +1,31 @@
+<?php
+session_start ();
+require('header.php');
+?>
+<div class="container">
+    <div class="grid-1 callout colums-small-6">
+        <form class="callout text-center">
+            <div class="floated-label-wrapper">
+                <label for="Auteur">Auteur</label>
+                <input type="text"  name="auteur" placeholder="Auteur">
+            </div>
+            <div class="floated-label-wrapper">
+                <label for="type">Type</label>
+                <input type="text"  name="type" placeholder="Type" required>
+            </div>
+            <div class="floated-label-wrapper">
+                <label for="pass">Description</label>
+                <input type="text"  name="description" placeholder="Description" required>
+            </div>
+            <input class="button expanded" type="submit" value="Valider">
+        </form>
+    </div>
+
+    <div class="grid-2 callout colums-small-6">
+    Affichage du média
+    </div>
+
+</div>
+<?php
+require('footer.php');
+?>
